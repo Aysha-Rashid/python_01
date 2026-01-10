de9f696eb4eddb2a8dbd184929daea5e75b0b943
Aysha-Rashid <aysharash6756@gmail.com>
2026-01-10 16:20:37 +0400

    from laptop

--- ex01/array2D.py
+++ ex01/array2D.py
@@ -4,9 +4,9 @@ def slice_me(family: list, start: int, end: int) -> list:
     """Takes a list, displays the shape of the list and returns a truncated version of the array based on the provided start and end argument"""
 
     try:
-        if not isinstance(family, list) or not isinstance(start, int) or not isinstance(end, int):
-            raise TypeError("invalid type")
         a = np.array(family, dtype=float)
+        if not isinstance(start, int) or not isinstance(end, int):
+            raise TypeError("invalid type")
         print("My shape is :", a.shape)
         sliceStore = a[start: end]
         print("My new shape is :", sliceStore.shape)
